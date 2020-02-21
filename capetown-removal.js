a997383983b73e0553fb13b01b3ab4d300e3ca78
mz1-mzone <61159139+mz1-mzone@users.noreply.github.com>
2020-02-22 00:21:23 +0300

    Create capetown-removal.js

---
+++ capetown-removal.js
@@ -0,0 +1,13 @@
+$("#removal-capetown").on('click', ()=>{  
+
+    cx = 0;
+    cy = 0;
+    cz = 0;
+  	$("#adults-numb-capetown").val("0");
+  	$("#children-numb-capetown").val("0");
+  	$("#kids-numb-capetown").val("0");
+    $("#passengers-total-capetown").val(" Adults: "+cx+" children: "+cy+" Kids: "+cz); 
+    cpr = (cx*0)+(cy*0)+(cz*0);
+    $("#price-capetown").val("Total Price:"+"€"+cpr); 
+  
+    document.querySelector('.capetown-totalprice').innerHTML = "Total: "+"€"+cpr;  //end of tour passengers
