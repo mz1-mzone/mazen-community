3a65eb71f14dd309d55113d7674506759f726b0b
mz1-mzone <61159139+mz1-mzone@users.noreply.github.com>
2020-03-01 18:18:48 +0300

    Create onepage.js

---
+++ onepage.js
@@ -0,0 +1,10 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.19/jquery.scrollify.min.js" integrity="sha256-RGlAwHKG24hsj1109SGQgvR+Orx7u8c6zyYH4Tk3ydU=" crossorigin="anonymous"></script>
+
+<script>
+	        $(function() {
+          $.scrollify({
+            section : ".section",
+          });
+        });
+
+</script>
